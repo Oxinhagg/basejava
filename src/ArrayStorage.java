74f66358a6cc6a8ab09e6789d091387b51cdfd22
Dmitry <46846350+Oxinhagg@users.noreply.github.com>
2020-05-13 21:53:20 +0300

    Update ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,55 @@
-/**
- * Array based storage for Resumes
+/*
+  Array based storage for Resumes
  */
+
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage)
+            if (resume != null && uuid.equals(resume.uuid)) {
+                return resume;
+            }
         return null;
     }
 
     void delete(String uuid) {
+        for (Resume resume : storage) {
+            if (resume != null && uuid.equals(resume.uuid)) {
+                resume.uuid = null;
+                break;
+            }
+        }
+    }
+
+    private Resume[] getArrNonNull() {
+        return Arrays.stream(storage).filter(Objects::nonNull).toArray(Resume[]::new);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return getArrNonNull();
     }
 
     int size() {
-        return 0;
+        return getArrNonNull().length;
     }
 }
