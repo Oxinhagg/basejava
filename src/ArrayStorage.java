3f47f48a43d922e332f9afaf76853521a67982cd
oxinhagg <dim.makhortoff@gmail.com>
2020-05-14 14:51:58 +0300

    refactor + correction

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,60 @@
-/**
- * Array based storage for Resumes
+/*
+  Array based storage for Resumes
  */
+
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int arrSize = 0;
 
     void clear() {
+        for (int i = 0; i < arrSize; i++) {
+            storage[i] = null;
+        }
+        arrSize = 0;
     }
 
     void save(Resume r) {
+        storage[arrSize] = r;
+        arrSize++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < arrSize; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int lastResume;
+        lastResume = arrSize - 1;
+        for (int i = 0; i < arrSize; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                if (i < lastResume) {
+                    storage[i] = storage[lastResume];
+                    storage[lastResume] = null;
+                } else {
+                    storage[i] = null;
+                }
+                arrSize--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.stream(storage).filter(Objects::nonNull).toArray(Resume[]::new);
     }
 
     int size() {
-        return 0;
+        return arrSize;
     }
 }
