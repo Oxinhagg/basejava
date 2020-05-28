44da9d046e438d0de26e9c6a3f994c2eaa9b3c76
oxinhagg <dim.makhortoff@gmail.com>
2020-05-28 21:09:02 +0300

    uuid check

--- src/com/oxinhagg/webapp/storage/ArrayStorage.java
+++ src/com/oxinhagg/webapp/storage/ArrayStorage.java
@@ -16,9 +16,14 @@ public class ArrayStorage {
             storage[i] = null;
         }
         arrSize = 0;
+
     }
 
     public void save(Resume r) {
+        String uuid = r.getUuid();
+        if (uuid_check(uuid)){
+            return;
+        }
         storage[arrSize] = r;
         arrSize++;
     }
@@ -29,11 +34,26 @@ public class ArrayStorage {
                 return storage[i];
             }
         }
+        System.out.println(String.format("Резюме с uuid = %s - не существует!", uuid));
         return null;
+    }
+
+    public void update(Resume resume){
+        String uuid = resume.getUuid();
+
+        if (!uuid_check(uuid)){
+            return;
+        }
+
     }
 
     public void delete(String uuid) {
         int lastResume = arrSize - 1;
+
+        if (!uuid_check(uuid)){
+            return;
+        }
+
         for (int i = 0; i < arrSize; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 if (i < lastResume) {
@@ -48,6 +68,14 @@ public class ArrayStorage {
         }
     }
 
+    private boolean uuid_check(String uuid){
+        if (get(uuid) != null){
+            System.out.println(String.format("Резюме с uuid = %s - уже существует!", uuid));
+            return true;
+        }
+        System.out.println(String.format("Резюме с uuid = %s - не существует!", uuid));
+        return false;
+}
     /**
      * @return array, contains only Resumes in storage (without null)
      */
