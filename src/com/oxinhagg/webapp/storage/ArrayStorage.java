04fcbe20b077be4217367bfd1bba481a136196c2
oxinhagg <dim.makhortoff@gmail.com>
2020-06-01 13:22:36 +0300

    add equals and hashCode + refactor method getAll

--- src/com/oxinhagg/webapp/storage/ArrayStorage.java
+++ src/com/oxinhagg/webapp/storage/ArrayStorage.java
@@ -19,19 +19,23 @@ public class ArrayStorage {
 
     public void save(Resume r) {
         String uuid = r.getUuid();
-        int index = getResumeIndex(uuid);
+        int index = getIndex(uuid);
 
         if (index != -1){
             System.out.println(String.format("Резюме с uuid = %s - уже существует!", uuid));
             return;
+        } else if (arrSize == storage.length) {
+            System.out.println("Массив резюме переполнен!");
+            return;
         }
+
 
         storage[arrSize] = r;
         arrSize++;
     }
 
     public Resume get(String uuid) {
-        int index = getResumeIndex(uuid);
+        int index = getIndex(uuid);
 
         if (index == -1) {
             System.out.println(String.format("Резюме с uuid = %s - не существует!", uuid));
@@ -43,7 +47,7 @@ public class ArrayStorage {
 
     public void update(Resume resume){
         String uuid = resume.getUuid();
-        int index = getResumeIndex(uuid);
+        int index = getIndex(uuid);
 
         if (index == -1){
             System.out.println(String.format("Резюме с uuid = %s - не существует!", uuid));
@@ -54,7 +58,7 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-        int index = getResumeIndex(uuid);
+        int index = getIndex(uuid);
 
         if (index == -1){
             System.out.println(String.format("Резюме с uuid = %s - не существует!", uuid));
@@ -66,7 +70,7 @@ public class ArrayStorage {
         storage[arrSize] = null;
     }
 
-    private int getResumeIndex(String uuid){
+    private int getIndex(String uuid){
         for (int i = 0; i < arrSize; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
@@ -79,7 +83,8 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        return Arrays.stream(storage).filter(Objects::nonNull).toArray(Resume[]::new);
+        //return Arrays.stream(storage).filter(Objects::nonNull).toArray(Resume[]::new);
+        return Arrays.copyOfRange(storage, 0, arrSize);
     }
 
     public int size() {
